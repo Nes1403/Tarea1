48f0e3ca7c92b121cea775d3ecedff3bcbcf2e4d
Nes <nes1403@ciencias.unam.mx>
2024-08-29 19:34:26 -0600

    ya esta

---
+++ app/src/main/java/com/example/tarea1/Tarea1.java
@@ -0,0 +1,15 @@
+package com.example.tarea1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Tarea1 extends Activity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+
+
+}
